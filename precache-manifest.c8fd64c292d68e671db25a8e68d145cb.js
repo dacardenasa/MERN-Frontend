34b401b2cd2d86ef9fa7835ed593db218a54bb8d
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-17 11:17:04 -0500

    Updates

---
+++ precache-manifest.c8fd64c292d68e671db25a8e68d145cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2e0e69fcb33dda2a3b9089051fefcf",
+    "url": "/MERN-Frontend/index.html"
+  },
+  {
+    "revision": "b30518d5d27bc2899b69",
+    "url": "/MERN-Frontend/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "49ad46dd3c310b4989f8",
+    "url": "/MERN-Frontend/static/css/main.19d421d9.chunk.css"
+  },
+  {
+    "revision": "b30518d5d27bc2899b69",
+    "url": "/MERN-Frontend/static/js/2.bb68d9bf.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/MERN-Frontend/static/js/2.bb68d9bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ad46dd3c310b4989f8",
+    "url": "/MERN-Frontend/static/js/main.e6d20dce.chunk.js"
+  },
+  {
+    "revision": "ddbe213944b105be2643",
+    "url": "/MERN-Frontend/static/js/runtime-main.6ac33cf8.js"
+  }
+]);
